0ed3fdd1a19cf75bb7d14df67fb343314aad7abf
Steven Wong <stevenwong1000777@gmail.com>
2019-11-07 21:27:12 -0500

    Generalized script and improved text

--- santa.py
+++ santa.py
@@ -6,8 +6,22 @@ def hasSame(a, b):
 			return True
 	return False
 
-people = ['Steven', 'Jeffrey', 'Lucy', 'Yuehan', 'Johnny', 'Delina', 'Jiashu', 'William']
-match = ['Steven', 'Jeffrey', 'Lucy', 'Yuehan', 'Johnny', 'Delina', 'Jiashu', 'William']
+# Assume correct usage
+numPeople = int(input("How many people are doing secret santa?\n"))
+if numPeople == 1:
+	print("You can't do secret santa by yourself :(.")
+	exit(-1)
+print("We have {} people. Now enter their names.".format(numPeople))
+people = []
+for _ in range(numPeople):
+	while True:
+		name = input()
+		if name in people:
+			print("{} is already in our list! Enter another name.".format(name))
+			continue
+		people.append(name)
+		break
+match = people[:] # make shallow copy
 
 while hasSame(people, match):
 	random.shuffle(match)
@@ -16,7 +30,7 @@ for i in range(len(people)):
 	from_name = people[i]
 	to_name = match[i]
 	f = open("{}.txt".format(from_name), "w+")
-	f.write("Hello {}, you are giving a present to {}!".format(from_name, to_name))
+	f.write("Hello {}, you are giving a present to {}!\n".format(from_name, to_name))
 	f.close()
 
 print("Have fun with Secret Santa! :)")
