307f16e9f5bc2cfdde1783ac5f0b458f70a30f00
Steven Wong <stevenwong1000777@gmail.com>
2021-01-08 18:47:34 +0800

    Add blacklist functionality

--- README.md
+++ README.md
@@ -1,14 +1,37 @@
 # secret-santa
 
-A simple to use Secret Santa generator *without* having to give your email(s) to random websites online.
+A simple to use Secret Santa generator _without_ having to give your email(s) to random websites online.
 This is done by having the script output text files, which you can then send your friends.
 The name on each outputted file is the person you should send that file to.
 
 🎁🎄🎁
 
 ## How to use
+
 1. `python santa.py`
 2. Follow the given instructions.
 3. Buy a gift for your assigned person.
 4. Give that person the gift.
 5. Have a Merry Christmas! ☃️
+
+Alternatively, you can prepare a file to pipe in the necessary information, e.g.
+
+```
+4
+Person1
+Person2
+Person3
+Person4
+```
+
+and then `python santa.py < file.txt`.
+
+If you want to introduce a blacklist, e.g. `Person1` cannot get `Person2` or `Person3`, you can modify the file like so:
+
+```
+4
+Person1: Person2, Person3
+Person2
+Person3
+Person4
+```
--- santa.py
+++ santa.py
@@ -6,24 +6,45 @@ def hasSame(a, b):
 			return True
 	return False
 
+def includedBlackList(people, match, blacklist):
+	for i in range(len(people)):
+		if match[i] in blacklist[people[i]]:
+			return True
+	return False
+
 # Assume correct usage
 numPeople = int(input("How many people are doing secret santa?\n"))
 if numPeople == 1:
 	print("You can't do secret santa by yourself :(.")
 	exit(-1)
-print("We have {} people. Now enter their names.".format(numPeople))
+print("We have {} people. Now enter their names and their blacklist.".format(numPeople))
+print("The format is 'Name1: BlacklistedName1, ..., BlacklistedNameN'")
 people = []
+blacklist = {}
 for _ in range(numPeople):
-	while True:
-		name = input()
-		if name in people:
-			print("{} is already in our list! Enter another name.".format(name))
-			continue
-		people.append(name)
-		break
+	input_ = input()
+	if ":" in input_:
+		name, blacklistString = map(lambda x: x.strip(), input_.split(":"))
+	else:
+		name = input_.strip()
+		blacklistString = None
+
+	if name in people:
+		print("{} is already in our list!".format(name))
+		exit(-1)
+	people.append(name)
+
+	if blacklistString is not None:
+		blacklistNames = map(lambda x: x.strip(), blacklistString.split(","))
+	else:
+		blacklistNames = []
+	blacklist[name] = set(blacklistNames)
+
+print(people, blacklist)
+
 match = people[:] # make shallow copy
 
-while hasSame(people, match):
+while hasSame(people, match) or includedBlackList(people, match, blacklist):
 	random.shuffle(match)
 
 for i in range(len(people)):
